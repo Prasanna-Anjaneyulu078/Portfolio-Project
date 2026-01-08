c7f0f7cc49790eefb03d88e669a2b4d48f948dc8
Prasanna-Anjaneyulu078 <prasannaanjaneyulu078@gmail.com>
2026-01-09 01:34:42 +0530

    Change API_URL to production endpoint

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './About.css';
 
-const API_URL = 'http://localhost:3002/api'
+const API_URL = 'https://prasanna-portfolio-admin.vercel.app/api'
 
 const About = () => {
   const [data, setData] = useState({
